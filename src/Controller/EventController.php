e0e135f31a18228c8d519090b9218c98818f70f8
fuflin <abdelilah.fakhour@outlook.fr>
2023-05-31 11:54:54 +0200

    ajout fonction add et show

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,19 +2,76 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Event;
+use App\Form\EventType;
+use App\Service\FileUploader;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EventController extends AbstractController
 {
+    // affichage de tous les events dans la base de données
     #[Route('/event', name: 'app_event')]
-    public function index(): Response
+
+    public function index(EntityManagerInterface $em): Response
     {
+        $events = $em->getRepository(Event::class)->findAll();
         return $this->render('event/index.html.twig', [
-            'controller_name' => 'EventController',
+            'events' => $events,
         ]);
     }
 
-    
+
+    //fonction pour ajouter un event 
+    #[Route('/event/add', name: 'add_event')]
+
+    public function add(EntityManagerInterface $em, Event $event = null, FileUploader $fileUploader, Request $request): Response
+    {
+        if(!$event){
+
+            $event = new Event();
+        }
+
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        // si (on a bien appuyer sur submit && que les infos du formalaire sont conformes au filter input qu'on aura mis)
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $event = $form->getData(); // hydratation avec données du formulaire / injection des valeurs saisies dans le form
+
+
+                $posterFile = $form->get('poster')->getData(); // hydratation d'un fichier
+
+                if ($posterFile) {
+
+                    $posterFileName = $fileUploader->upload($posterFile); // on utilise le service upload pour chargé l'image afin de la stocké en base de donnée
+                    $event->setPoster($posterFileName);
+                }
+
+            $em->persist($event); // équivalent du prepare dans PDO
+            $em->flush(); // équivalent de insert into (execute) dans PDO
+
+            return $this->redirectToRoute('app_event');
+        }
+
+        // vue pour afficher le formulaire d'ajout
+        return $this->render('event/add.html.twig', [
+            'eventForm' => $form->createView()
+        ]); // création du formulaire
+    }
+
+
+    // fonction pour afficher les détails d'un event
+    #[Route('/event/{id}', name: 'show_event')]
+
+    public function show(Event $event): Response
+    {
+        return $this->render('event/detailEvent.html.twig', [
+           'event' => $event,
+        ]);
+    }
 }
