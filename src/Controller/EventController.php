a0e2b57c96811e11d8508d0b8a925f330c9a63c4
fuflin <abdelilah.fakhour@outlook.fr>
2023-05-31 16:59:51 +0200

    ajout fonction edit et delete

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -25,7 +25,7 @@ class EventController extends AbstractController
     }
 
 
-    //fonction pour ajouter un event 
+    //fonction pour ajouter un event
     #[Route('/event/add', name: 'add_event')]
 
     public function add(EntityManagerInterface $em, Event $event = null, FileUploader $fileUploader, Request $request): Response
@@ -62,6 +62,54 @@ class EventController extends AbstractController
         return $this->render('event/add.html.twig', [
             'eventForm' => $form->createView()
         ]); // création du formulaire
+    }
+
+    //fonction pour modifier un event
+    #[Route('/event/{id}/edit', name: 'edit_event')]
+
+    public function edit(EntityManagerInterface $em, Event $event = null, FileUploader $fileUploader, Request $request): Response
+    {
+
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        // si (on a bien appuyer sur submit && que les infos du formalaire sont conformes au filter input qu'on aura mis)
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $event = $form->getData(); // hydratation avec données du formulaire / injection des valeurs saisies dans le form
+
+
+                $posterFile = $form->get('poster')->getData(); // hydratation d'un fichier
+
+                if ($posterFile) {
+
+                    $posterFileName = $fileUploader->upload($posterFile); // on utilise le service upload pour chargé l'image afin de la stocké en base de donnée
+                    $event->setPoster($posterFileName);
+                }
+
+            $em->persist($event); // équivalent du prepare dans PDO
+            $em->flush(); // équivalent de insert into (execute) dans PDO
+
+            return $this->redirectToRoute('app_event');
+        }
+
+        // vue pour afficher le formulaire d'ajout
+        return $this->render('event/add.html.twig', [
+            'eventForm' => $form->createView(),
+            'edit'
+        ]); // création du formulaire
+    }
+
+    // fonction pour supprimer un event
+    #[Route('/event/{id}/delete', name: 'delete_event')]
+
+    public function delete(EntityManagerInterface $em, Event $event): Response
+    {
+
+        $em->remove($event);
+        $em->flush();
+
+        return $this->redirectToRoute('app_event');
     }
 
 
