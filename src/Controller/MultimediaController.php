4423294359096f71b195e41361e351a3ed86042f
fuflin <121032990+fuflin@users.noreply.github.com>
2023-06-04 22:09:48 +0200

    img test affichage + ajout photo galerie

---
+++ src/Controller/MultimediaController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Multimedia;
+use App\Form\MultimediaType;
+use App\Service\FileUploader;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MultimediaController extends AbstractController
+{
+    #[Route('/multimedia', name: 'app_multimedia')]
+    public function index(EntityManagerInterface $em): Response
+    {
+        $medias = $em->getRepository(Multimedia::class)->findAll();
+        return $this->render('multimedia/index.html.twig', [
+            'medias' => $medias,
+        ]);
+    }
+
+
+    //fonction pour ajouter un media
+    #[Route('/multimedia/add', name: 'add_media')]
+
+    public function add(EntityManagerInterface $em, Multimedia $media = null, FileUploader $fileUploader, Request $request): Response
+    {
+        if(!$media){
+
+            $media = new Multimedia();
+        }
+
+        $form = $this->createForm(MultimediaType::class, $media);
+        $form->handleRequest($request);
+
+        // si (on a bien appuyer sur submit && que les infos du formalaire sont conformes au filter input qu'on aura mis)
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $media = $form->getData(); // hydratation avec données du formulaire / injection des valeurs saisies dans le form
+
+
+                $posterFile = $form->get('media')->getData(); // hydratation d'un fichier
+
+                if ($posterFile) {
+
+                    $posterFileName = $fileUploader->upload($posterFile); // on utilise le service upload pour chargé l'image afin de la stocké en base de donnée
+                    $media->setMedia($posterFileName);
+                }
+
+            $em->persist($media); // équivalent du prepare dans PDO
+            $em->flush(); // équivalent de insert into (execute) dans PDO
+
+            return $this->redirectToRoute('app_multimedia');
+        }
+
+        // vue pour afficher le formulaire d'ajout
+        return $this->render('multimedia/add.html.twig', [
+            'mediaForm' => $form->createView()
+        ]); // création du formulaire
+    }
+}
