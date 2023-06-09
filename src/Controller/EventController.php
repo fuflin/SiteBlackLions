35dc50911782233c8ce681b7210518d0ec17c80e
fuflin <121032990+fuflin@users.noreply.github.com>
2023-06-09 15:14:11 +0200

    ...

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -112,40 +112,59 @@ class EventController extends AbstractController
     }
 
 
-    // fonction pour afficher les détails d'un event + inscription à l'event en question
+    // fonction pour afficher les détails d'un event
     #[Route('/event/{id}', name: 'show_event')]
 
-    public function showEvent(EntityManagerInterface $em, Event $event, Request $request): Response
+    public function showEvent(Event $event): Response
     {
 
-        $user = $this->getUser(); //on récupère le User en session
-
-        $participate = new Participate();
-
-        $form = $this->createForm(ParticipateType::class, $participate);
-        $form->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()) {
-
-                if(!$user){ // si la personne n'a pas de compte , elle est renvoyé vers la page d'inscription
-                    return $this->redirectToRoute('app_login');
-                }
-
-                $participate = $form->getData(); // hydratation avec données du formulaire / injection des valeurs saisies dans le form
-
-                $participate->setDateRegis(new \DateTime());
-                $participate->addUser($user);
-                $participate->addEvent($event);
-
-                $em->persist($participate); // équivalent du prepare dans PDO
-                $em->flush(); // équivalent de insert into (execute) dans PDO
-
-                return $this->redirectToRoute('app_event');
-            }
+        $participate = $event->getParticipates();
 
         return $this->render('event/detailEvent.html.twig', [
            'event' => $event,
-           'Form' => $form->createView(),
+           'participate' => $participate
         ]);
     }
+
+    // fonction pour s'inscrire à un événement
+    #[Route('/event/{id}/regis', name: 'regis_event')]
+
+    public function regisEvent(EntityManagerInterface $em, Event $event)
+    {
+        $user = $this->getUser(); //on récupère le User en session
+
+        if(!$user){ // si la personne n'a pas de compte , elle est renvoyé vers la page d'inscription
+            return $this->redirectToRoute('app_login');
+        }
+
+        $participate = new Participate();
+
+            $participate->setDateRegis(new \DateTime());
+            $participate->addUser($user);
+            $participate->addEvent($event);
+
+            $em->persist($participate); // équivalent du prepare dans PDO
+            $em->flush(); // équivalent de insert into (execute) dans PDO
+
+            return $this->redirectToRoute('app_event');
+    }
+
+    // fonction pour désinscrire à un événement
+    #[Route('/event/{id}/remove/{idParticipate}', name: 'remove_regis_event')]
+
+    public function removeRegisEvent(EntityManagerInterface $em, Event $event, $idParticipate)
+    {
+        $participate = $em->getRepository(Participate::class)->find($idParticipate);
+
+        $user = $this->getUser(); //on récupère le User en session
+
+        if($user){
+
+            $em->remove($participate);
+            $em->flush(); // équivalent de insert into (execute) dans PDO
+
+            return $this->redirectToRoute('app_event');
+        }
+    }
+
 }
