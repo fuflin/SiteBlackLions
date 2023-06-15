30f3382f74f1ac15128c0ca9efaa1c0d5b348409
fuflin <Abdelilah.Fakhour@outlook.fr>
2023-06-15 16:52:07 +0200

    préparation fonction ban user

--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
