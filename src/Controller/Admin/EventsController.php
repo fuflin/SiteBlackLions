9cd719dd9f3ca6ee12084b5968ca6370b6938b86
fuflin <121032990+fuflin@users.noreply.github.com>
2023-06-20 19:12:28 +0200

    ajout fonction admin

--- src/Controller/Admin/EventsController.php
+++ src/Controller/Admin/EventsController.php
@@ -3,8 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Event;
+use App\Form\EventType;
 use App\Service\SendMail;
+use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,15 +27,92 @@ class EventsController extends AbstractController
         return $this->render('admin/events/index.html.twig', compact('events'));
     }
 
+     //fonction pour ajouter un event
+     #[Route('/event/add', name: 'add_event')]
 
-    
-    #[Route('/event/{id}', name: 'delete')]
+     public function addEvent(EntityManagerInterface $em, Event $event = null, FileUploader $fileUploader, Request $request): Response
+     {
+         $event = new Event();
+ 
+         $form = $this->createForm(EventType::class, $event);
+         $form->handleRequest($request);
+ 
+         // si (on a bien appuyer sur submit && que les infos du formalaire sont conformes au filter input qu'on aura mis)
+         if ($form->isSubmitted() && $form->isValid()) {
+ 
+             $event = $form->getData(); // hydratation avec données du formulaire / injection des valeurs saisies dans le form
+ 
+                 $posterFile = $form->get('poster')->getData(); // hydratation d'un fichier
+ 
+                 if ($posterFile) {
+ 
+                     $posterFileName = $fileUploader->upload($posterFile); // on utilise le service upload pour chargé l'image afin de la stocké en base de donnée
+                     $event->setPoster($posterFileName);
+                 }
+ 
+             $em->persist($event); // équivalent du prepare dans PDO
+             $em->flush(); // équivalent de insert into (execute) dans PDO
+ 
+             return $this->redirectToRoute('app_event');
+         }
+ 
+         // vue pour afficher le formulaire d'ajout
+         return $this->render('event/add.html.twig', [
+             'eventForm' => $form->createView()
+         ]); // création du formulaire
+    }
 
-    public function sendMail(Event $event, SendMail $mail):Response
+    //fonction pour modifier un event
+    #[Route('/event/{id}/edit', name: 'edit_event')]
+
+    public function editEvent(EntityManagerInterface $em, Event $event = null, FileUploader $fileUploader, Request $request): Response
     {
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        // si (on a bien appuyer sur submit && que les infos du formalaire sont conformes au filter input qu'on aura mis)
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $event = $form->getData(); // hydratation avec données du formulaire / injection des valeurs saisies dans le form
+
+
+                $posterFile = $form->get('poster')->getData(); // hydratation d'un fichier
+
+                if ($posterFile) {
+
+                    $posterFileName = $fileUploader->upload($posterFile); // on utilise le service upload pour chargé l'image afin de la stocké en base de donnée
+                    $event->setPoster($posterFileName);
+                }
+
+            $em->persist($event); // équivalent du prepare dans PDO
+            $em->flush(); // équivalent de insert into (execute) dans PDO
+
+            return $this->redirectToRoute('admin_events_index');
+        }
+
+        // vue pour afficher le formulaire d'ajout
+        return $this->render('event/add.html.twig', [
+            'eventForm' => $form->createView(),
+            'edit'
+        ]); // création du formulaire
+    }
+
+    // fonction pour supprimer un event
+    #[Route('/event/{id}/delete', name: 'delete_event')]
+
+    public function deleteEvent(EntityManagerInterface $em, Event $event, SendMail $mail): Response
+    {
+
         dd($event);
 
+        // $em->remove($event);
+        // $em->flush();
+
         return $this->redirectToRoute('admin_events_index');
+
     }
 
 }
