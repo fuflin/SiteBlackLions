737a32f6c4f1557a8498ffe3783356eb3c0244ea
fuflin <121032990+fuflin@users.noreply.github.com>
2023-06-22 20:26:03 +0200

    ajout edit pseudo, mail, delete et chang password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,32 +3,35 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
+use App\Form\UserType;
+use App\Form\UserMailType;
+use App\Form\ChangPasswordType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
+
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        return $this->render('user/index.html.twig', []);
     }
 
-    //fonction pour modifier un user
-    #[Route('/user/{id}/edit', name: 'edit_user')]
 
-    public function editEvent(EntityManagerInterface $em, User $user, Request $request): Response
+    //fonction pour modifier le pseudo ou le pseudo discord
+    #[Route('/user/{id}/edit_nickname', name: 'edit_user_pseudo')]
+
+    public function editNickname(EntityManagerInterface $em, User $user, Request $request): Response
     {
 
-        // $user = $this->getUser();
+        $user = $this->getUser();
 
-        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         // si (on a bien appuyer sur submit && que les infos du formalaire sont conformes au filter input qu'on aura mis)
@@ -43,9 +46,81 @@ class UserController extends AbstractController
         }
 
         // vue pour afficher le formulaire d'ajout
-        return $this->render('user/index.html.twig', [
-            'registrationForm' => $form->createView(),
+        return $this->render('user/editPseudo.html.twig', [
+            'pseudoForm' => $form->createView(),
             'edit'
         ]); // création du formulaire
+    }
+
+    //fonction pour modifier le pseudo ou le pseudo discord
+    #[Route('/user/{id}/edit_email', name: 'edit_user_mail')]
+
+    public function editMail(EntityManagerInterface $em, User $user, Request $request): Response
+    {
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserMailType::class, $user);
+        $form->handleRequest($request);
+
+        // si (on a bien appuyer sur submit && que les infos du formalaire sont conformes au filter input qu'on aura mis)
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData(); // hydratation avec données du formulaire / injection des valeurs saisies dans le form
+
+            $em->persist($user); // équivalent du prepare dans PDO
+            $em->flush(); // équivalent de insert into (execute) dans PDO
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        // vue pour afficher le formulaire d'ajout
+        return $this->render('user/editEmail.html.twig', [
+            'mailForm' => $form->createView(),
+            'edit'
+        ]); // création du formulaire
+    }
+
+    #[Route('/user/{id}/edit_password', name: 'edit_user_password')]
+
+    public function editPassword(EntityManagerInterface $em, User $user, UserPasswordHasherInterface $hasher, Request $request): Response
+    {
+
+        // $user = $this->getUser();
+
+        $form = $this->createForm(ChangPasswordType::class);
+        $form->handleRequest($request);
+
+        // si (on a bien appuyer sur submit && que les infos du formalaire sont conformes au filter input qu'on aura mis)
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd($form->getData());
+            if($hasher->isPasswordValid($user, $form->getData()->getPlainPassword())) {
+
+                $user->setPassword($form->getData()->getNewPassword());
+            }
+
+            $em->persist($user); // équivalent du prepare dans PDO
+            $em->flush(); // équivalent de insert into (execute) dans PDO
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        // vue pour afficher le formulaire d'ajout
+        return $this->render('user/editPassword.html.twig', [
+            'passForm' => $form->createView(),
+            'edit'
+        ]); // création du formulaire
+    }
+
+    #[Route('/event/{id}/delete_user', name: 'delete_user')]
+
+    public function deleteEvent(EntityManagerInterface $em, User $user): Response
+    {
+        $user = $this->getUser();
+
+        $em->remove($user);
+        $em->flush();
+
+        return $this->redirectToRoute('app_home');
     }
 }
--- src/Form/ChangPasswordType.php
+++ src/Form/ChangPasswordType.php
@@ -11,13 +11,25 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class ChangPasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
 
-            ->add('oldPassword', PasswordType::class)
+            ->add('plainPassword', PasswordType::class, [
+                'attr' => ['class' => 'form-control'],
+                'label' => 'Ancien mot de passe',
+                // 'first_options' => [
+                //     'constraints' => [
+                //         new NotBlank([
+                //             'message' => 'Please enter a password',
+                //         ]),
+                //     ]
+                // ],
+            ])
 
             ->add('newPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
@@ -48,6 +60,38 @@ class ChangPasswordType extends AbstractType
                 // this is read and encoded in the controller
                 'mapped' => false,
             ])
+
+            // ->add('plainPassword', RepeatedType::class, [
+            //     // instead of being set onto the object directly,
+            //     // this is read and encoded in the controller
+            //     'mapped' => false,
+            //     'type' => PasswordType::class,
+            //     'attr' => ['autocomplete' => 'new-password'],
+            //     'options' => ['attr' => ['class' => 'password-field']],
+            //         'required' => true,
+            //         'attr'=> [
+            //             'class' => 'form-control',
+            //             ],
+            //         'first_options' => ['label' => 'Mot de passe', 'attr' =>['placeholder' => '12 characters min']],
+            //         'second_options' => ['label' => 'Confirmer le mot de passe', 'attr' =>['placeholder' => '12 characters min']],
+            //     'constraints' => [
+            //         new NotBlank([
+            //             'message' => 'Entrez un mot de passe',
+            //         ]),
+            //         // new Regex([ //la regex impose des conditions pour le mdp: 1 majuscule, 1 minuscule, 1 nombre, 1 charactère spéciale
+            //         //     'pattern' => '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,32}$',
+            //         //     'match' => true,
+            //         //     'message' => 'Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un nombre, un caractère spéciale compris entre 12 et 32 caractères'
+            //         //     ]),
+            //         ],
+            //     ])
+
+            //     ->add('newPassword', PasswordType::class, [
+            //         'attr' => ['class' => 'form-control'],
+            //         'label' => 'Nouveau mot de passe',
+            //         'label_attr' => ['class' => 'form-label mt-4'],
+            //         // 'constrains' => [new Assert\NotBlank()]
+            //     ])
 
             ->add('submit', SubmitType::class, [
                 'attr' => ['class' => 'btn btn-primary']])
