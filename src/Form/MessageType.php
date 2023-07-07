11ec1fdf1788cd0bbc935648d0d11096560699a4
fuflin <121032990+fuflin@users.noreply.github.com>
2023-07-07 14:57:56 +0200

    ajout élément pour le message type

--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -2,10 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -14,11 +17,25 @@ class MessageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+
+            ->add('title', TextType::class, ['attr'=> [
+                'class' => 'form-control',
+                'placeholder' => "Titre du message"],
+                'label' => 'Titre'])
+
             ->add('text', TextareaType::class, [
                 'attr'=> [
                 'class' => 'form-control',
                 'placeholder' => "Saisissez votre message"],
-                'label' => 'Message'])
+                'label' => 'Contenu du message'])
+
+            ->add('received', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'email',
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
 
             ->add('submit', SubmitType::class, [
                 'attr' => ['class' => 'btn btn-primary']])
