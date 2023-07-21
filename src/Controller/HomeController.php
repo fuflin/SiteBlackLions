6681509f4e084ab0074509fcce109eba6b676eff
fuflin <Abdelilah.Fakhour@outlook.fr>
2023-07-21 14:23:52 +0200

    ...

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Event;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+
+        $events = $em->getRepository(Event::class)->findBy([], ['date_create' => 'DESC'], 3);
+
         return $this->render('home/index.html.twig', [
             'description' => "ceci est ma meta description pour la page d'accueil",
+            'events' => $events,
         ]);
     }
 
